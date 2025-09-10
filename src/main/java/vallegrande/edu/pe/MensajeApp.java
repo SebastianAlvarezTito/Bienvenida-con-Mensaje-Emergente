577813d7461ccc3f76225ca6e05758b318bc000b
Sebastian-Alvarez <sebastian.alvarez@vallegrande.edu.pe>
2025-09-10 09:53:56 -0500

    Update MensajeApp.java

--- src/main/java/vallegrande/edu/pe/MensajeApp.java
+++ src/main/java/vallegrande/edu/pe/MensajeApp.java
@@ -1,1 +1,19 @@
+package vallegrande.edu.pe.view;
 
+import vallegrande.edu.pe.controller.MensajeController;
+import vallegrande.edu.pe.model.MensajeModel;
+
+import javax.swing.*;
+
+public class MensajeApp {
+    public static void main(String[] args) {
+        // Use SwingUtilities.invokeLater to ensure the GUI is created on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            MensajeModel model = new MensajeModel();
+            MensajeView view = new MensajeView();
+            MensajeController controller = new MensajeController(model, view);
+
+
+        });
+    }
+}
