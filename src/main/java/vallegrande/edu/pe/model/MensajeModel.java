8f903c5074b834c695a3bad4e6801379dca5721b
Sebastian-Alvarez <sebastian.alvarez@vallegrande.edu.pe>
2025-09-10 09:52:20 -0500

    Create MensajeModel.java

---
+++ src/main/java/vallegrande/edu/pe/model/MensajeModel.java
@@ -0,0 +1,13 @@
+package vallegrande.edu.pe.model;
+
+public class MensajeModel {
+    private String nombreUsuario;
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+}
